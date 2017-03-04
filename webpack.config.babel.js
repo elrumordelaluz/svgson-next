df9c3b820abaee96f06f718e23c03bd9bd2d8039
Lionel T <elrumordelaluz@hotmail.com>
2017-03-04 12:19:32 +0100

    build(webpack): Add webpack 2 to compile to use in Browser

---
+++ webpack.config.babel.js
@@ -0,0 +1,22 @@
+import { join } from 'path'
+
+const include = join(__dirname, 'src')
+
+export default {
+  entry: './src/index',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'svgson',
+  },
+  externals: {
+    'svgo': 'svgo'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', include },
+      { test: /\.json$/, loader: 'json-loader', include },
+    ],
+  }
+} 
